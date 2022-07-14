58ae7ebbcd41873c741321b5ddacd3c19ffc4f35
OMKARGOPCHADE <86941500+OMKARGOPCHADE@users.noreply.github.com>
2022-07-14 19:19:42 +0530

    Update date.c

--- First Assignment/date.c
+++ First Assignment/date.c
@@ -5,13 +5,8 @@ int main()
     printf("Enter a date,month and year as given format\n");
     printf("DD/MM/YYY/\n");
     scanf("%d/%d/%d",&d,&m,&y);
-     if(d<10&&m<10)
-    printf("\n Day - 0%d ,Month - 0%d,Year - %d",d,m,y);
-  else  if(d<10)
-    printf("\n Day - 0%d ,Month - %d,Year - %d",d,m,y);
-    else if(m<10)
-    printf("\n Day - %d ,Month - 0%d,Year - %d",d,m,y);
-        else 
-    printf("\n Day - %d ,Month - %d,Year - %d",d,m,y);
+
+
+printf("\n Day - %d ,Month - %d,Year - %d",d,m,y);
     return 0;
 }
