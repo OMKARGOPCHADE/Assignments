e614b2a9e557b73ce0b62a5aeecf383d929ee1ac
OMKARGOPCHADE <86941500+OMKARGOPCHADE@users.noreply.github.com>
2022-07-14 19:29:12 +0530

    Update nine.c

--- Second Assignment/nine.c
+++ Second Assignment/nine.c
@@ -7,7 +7,7 @@ int main()
     printf("enter a digt: ");
     scanf("%d",&d);
     printf("befor adding the digit number is %d", x);
-     x = x * 10 + (x < d? 0 : d);
+     x = x * 10 + d;
     printf("\nafter adding a digit the number is : %d", x);
     return 0;
 }
