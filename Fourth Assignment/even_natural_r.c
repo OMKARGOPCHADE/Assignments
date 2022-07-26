d547443d6e3a2be315e6df6eb4cca02451c2d9aa
OMKARGOPCHADE <86941500+OMKARGOPCHADE@users.noreply.github.com>
2022-07-26 14:06:36 +0530

    Add files via upload

---
+++ Fourth Assignment/even_natural_r.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=20;
+   do
+    {
+        if(i%2==0)
+       printf("%d\n",i);
+       i--;
+    } while (i>=1);
+    
+    return 0;
+}
---
+++ Fourth Assignment/odd_natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+   do
+    {
+       printf("%d\n",i);
+       i+=2;
+      
+    } while (i<=20);
+    
+    return 0;
+}
